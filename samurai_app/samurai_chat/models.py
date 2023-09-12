0e5b8891c4dc2c61d388357d013cfdb8d5baf164
Astryda Malinowska <astamalinowska@outlook.com>
2023-09-12 22:14:35 +0200

    Created models for user and message

--- samurai_app/samurai_chat/models.py
+++ samurai_app/samurai_chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+class Message(models.model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
