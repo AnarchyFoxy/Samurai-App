5e6b28a8963747a3b73c702dec9777401c13500f
Astryda Malinowska <astamalinowska@outlook.com>
2023-09-12 22:29:23 +0200

    created templates for registration and login

--- samurai_app/samurai_chat/views.py
+++ samurai_app/samurai_chat/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('samurai_chat')
+    return render(request, 'registration/login.html')
